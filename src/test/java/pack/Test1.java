75199faf3f891d5b2ad6f8a92807f4968f5f8837
kalyan <kalyanchakravarthipathella@gamil.com>
2022-04-07 18:43:05 +0530

    launch sit'

---
+++ src/test/java/pack/Test1.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		WebDriverManager.chromedriver().setup();
+		RemoteWebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(5000);
+
+	}
+
+}
